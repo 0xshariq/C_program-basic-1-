8cc40f229e778e68c44fa739a76ef67fc82cbb30
0xshariq <145042629+0xshariq@users.noreply.github.com>
2024-01-29 15:33:59 +0000

    New

--- print_integer.c
+++ print_integer.c
@@ -1,14 +1,10 @@
-// C Program to Print
-// Integer value
+// C Program to Print Integer value
 #include <stdio.h>
- 
-// Driver code
+
 int main()
 {
-    // Declaring integer
+    
     int x = 5;
- 
-    // Printing values
     printf("Printing Integer value %d", x);
     return 0;
 }
--- printname.c
+++ printname.c
@@ -1,5 +1,4 @@
-// C program to demonstrate printing of 
-// our own name using scanf() 
+
 #include <stdio.h> 
   
 int main() 
@@ -7,7 +6,7 @@ int main()
     char name[20]; 
     printf("Enter name: "); 
   
-    // user input will be taken here 
+     
     scanf("%s", name); 
     printf("Your name is %s.", name); 
     return 0; 
